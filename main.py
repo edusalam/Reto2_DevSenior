b9bbb5add2ec33a990af822e329067750fcd3514
edusalam <eduarfernando.salcedo@gmail.com>
2025-02-25 23:15:18 -0500

    RETO ACTUALIZADO CON EXITO 25/02/2025

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ clientes = [] #VARIABLE GLOBAL:
 mascotas = []
 
 class SistemaVeterinaria:
-    pass
+    
     class Persona:
         id_counter = 1 #CONTADOR ES AUTOMATICAMENTE AUTOINCREMENTABLE
 
@@ -21,41 +21,48 @@ class SistemaVeterinaria:
             self.mascotas = [] 
 
         def agregar_mascota(self, mascota):
-            self.mascotas.append(mascota) 
+            self.mascotas.append(mascota)
 
     class Mascota:
         id_counter = 1
         def __init__(self, nombre, especie, raza, edad):
-            SistemaVeterinaria.mascota.id_counter += 1
+            self.id = SistemaVeterinaria.Mascota.id_counter
             self.nombre = nombre
             self.especie = especie
             self.raza = raza
             self.edad = edad
             self.historia_clinico = []
+
             SistemaVeterinaria.Mascota.id_counter += 1    
 
         def agregar_cita(self,cita):
             self.historia_clinico.append(cita)
-
-
-    class Citas:
+        
+        def mostrar_historial(self):
+            if not self.historia_clinico:
+                print(f'no hay citas registradas para {self.nombre}')
+            else:
+                print(f'historial de las citas para {self.nombre}')
+                for cita in self.historia_clinico:
+                    print(f'- {cita.fecha} a las {cita.hora}, servicio: {cita.servicio}, veterinario{cita.veterinario}')
+    class Cita:
         id_counter = 1
         def __init__(self, fecha, hora, servicio, veterinario):
-            self.id = SistemaVeterinaria.Citas.id_counter
+            self.id = SistemaVeterinaria.Cita.id_counter
             self.fecha = fecha
             self.hora = hora
             self.servicio = servicio
             self.veterinario = veterinario
-            SistemaVeterinaria.Citas.id_counter += 1
+
+            SistemaVeterinaria.Cita.id_counter += 1
 #funciones auxiliares
 def validar_fecha(fecha):
     from datetime import datetime
     try:
-        datetime.strptime(fecha,'%d-%d-%Y')
+        datetime.strptime(fecha,'%Y-%m-%d')
         return True
     except ValueError:
-        return False
-      
+        return False      
 
 #CREAMOS POR FUERA LOS METODOS O FUNCINES DEL SISTEMA VETERINARIO
 def registrar_Cliente():
@@ -78,7 +85,7 @@ def registrar_mascota():
     especie = input('Especie de la mascota: ')
     raza = input('Raza de la mascota: ')
     edad = input('Ingrese edad de la mascota: ')
-    mascota = SistemaVeterinaria.Mascota(nombre_mascota,especie, raza, edad)
+    mascota = SistemaVeterinaria.Mascota(nombre_mascota,especie,raza,edad)
 
     cliente.agregar_mascota(mascota)
     mascotas.append(mascota)
@@ -90,27 +97,42 @@ def programar_cita():
     cliente = next((c for c in clientes if c.id == cliente_id), None) #SENTENCIA
 
     if not cliente:
-            print('cliente no encontrado')
-            return
-    mascota_id = int(input('ingrese el id del cliente: '))
+        print('cliente no encontrado')
+        return
+    mascota_id = int(input('ingrese el id del mascota: '))
     mascota = next((m for m in cliente.mascotas if m.id == mascota_id), None)
 
     if not mascota:
         print('mascota no encontrado')
         return
-    fecha = input('ingrese la fecha de la cita(DD-MM-YYYY):')
+    fecha = input('ingrese la fecha de la cita(YYYY-MM-DD):')
     while not validar_fecha(fecha):
-        print('fecha invalida, por favor use el formato DD-MM-YYYY ')
-        fecha = input('ingrese la fecha de la cita(DD-MM-YYYY):')
+        print('fecha invalida, por favor use el formato YYYY-MM-DD ')
+        fecha = input('ingrese la fecha de la cita(YYYY-MM-DD):')
     hora = input('ingrese la hora del cita (HH:MM)')
     servicio = input('ingrese el servicio(consultorio, vacunacion, etc,):')
-    veterinario = input('ingrese el nombre del veterinario')
-
-
+    veterinario = input('ingrese el nombre del veterinario: ')
+    cita = SistemaVeterinaria.Cita(fecha,hora,servicio,veterinario)
+    mascota.agregar_cita(cita)
     print('Cita agendada')
 
-def consutlar_historial():
-        pass
+def consultar_historial():
+    print('====consultar historial de citas====')
+    cliente_id = int(input('ingrese el id del cliente: '))
+    cliente = next((c for c in clientes if c.id == cliente_id), None) #SENTENCIA
+
+    if not cliente:
+        print('cliente no encontrado')
+        return
+    mascota_id = int(input('ingrese el id del mascota: '))
+    mascota = next((m for m in cliente.mascotas if m.id == mascota_id), None)
+
+    if not mascota:
+        print('mascota no encontrado')
+        return
+
+    mascota.mostrar_historial()
+
 
 #menu principal
 def menu_principal():
@@ -132,7 +154,8 @@ def menu_principal():
                 programar_cita()
             
             elif opc == '4':
-                consutlar_historial
+                consultar_historial()
+
             elif opc == '5':
                 break
 
